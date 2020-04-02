bb8cd356c0f0e74fcde373f5b67896e4f0df0483
Woom <theroyalblah@gmail.com>
2020-04-02 14:03:35 -0400

    subTask Column queries subtasks

--- src/components/subTaskColumn.tsx
+++ src/components/subTaskColumn.tsx
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
+import { SubTask } from "./subtaskType";
 import { SubTaskButton } from './subTaskButton';
 
 const styles = {
@@ -29,32 +30,77 @@ interface SubTaskColumnProps {
     changeHead: (newHead: number) => any
 }
 
-const tempTask = [
-    {title: "name1", progress: 50, _id: 99}
-];
-
 // This creates the entire right-hand column of project page. It handles the button that creates a new task,
 // and hands down a single element of a subtask array to create subtask buttons one by one.
-export class SubTaskColumn extends React.Component<SubTaskColumnProps>{
+export class SubTaskColumn extends React.Component<SubTaskColumnProps, { error: any, isLoaded: boolean, subTasks: SubTask[] }>{
 
     constructor(props: SubTaskColumnProps) {
         super(props);
+
+        this.state = {
+            error: null,
+            isLoaded: false,
+            subTasks: [],
+        };
+
+    }
+
+    componentDidMount() {
+        this.getSubtasks();
+    }
+
+    componentDidUpdate() {
+        this.getSubtasks();
+    }
+
+    getSubtasks = () => {
+        fetch(`https://tasktabs-backend.herokuapp.com/api/subtasks/${this.props.head}`)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    subTasks: result,
+                });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        );
     }
 
     render() {
-        return (
-            <Container>
-                <Col style={styles.box}>
-                    <Row noGutters={true}>
-                        <Button style={styles.button} size="lg" variant="outline-primary"> + New Task </Button>
-                    </Row>
-                    <Row noGutters={true}>
-                        {tempTask.map((task) => {
-                            return <SubTaskButton name={task.title} percentage={task.progress} key={task._id} changeHead={this.props.changeHead} taskHead={this.props.head}></SubTaskButton>;
-                        })}
-                    </Row>
-                </Col>
-            </Container>
-        );
+        const { error, isLoaded, subTasks } = this.state;
+        // TODO Style error and loading screens
+        if (error) {
+            return (
+                <>Error!</>
+            );
+        } else if (!isLoaded) {
+            return (
+                <>Loading...</>
+            );
+        } else {
+            return (
+                <Container>
+                    <Col style={styles.box}>
+                        <Row noGutters={true}>
+                            <Button style={styles.button} size="lg" variant="outline-primary"> + New Task </Button>
+                        </Row>
+                        <Row noGutters={true}>
+                            {subTasks.map((task) => {
+                                return <SubTaskButton name={task.title} percentage={task.progress} key={task._id} changeHead={this.props.changeHead} taskHead={task._id}></SubTaskButton>;
+                            })}
+                        </Row>
+                    </Col>
+                </Container>
+            );
+        }
     }
 };
