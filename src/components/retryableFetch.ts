c982092a5431508b7497fcf3bbcf6b96effcc664
unknown <justinlagree@live.com>
2020-04-16 15:51:47 -0400

    Use correct function syntax.

--- src/components/retryableFetch.ts
+++ src/components/retryableFetch.ts
@@ -4,13 +4,13 @@ export class RetryableFetch{
     //recursively call fetch_retry until there is a successful request or the last attempt is error
     //modified from : https://dev.to/ycmjason/javascript-fetch-retry-upon-failure-3p6g
     public static fetch_retry(request: RequestInfo, init: RequestInit = { }, n: number = 5): Promise<Response> {
-        return new Promise(function(resolve, reject) {
+        return new Promise((resolve, reject) => {
             fetch(request, init)
-                .then(function(result) {
+                .then((result) => {
                     /* on success */
                     resolve(result);
                 })
-                .catch(function(error) {
+                .catch((error) => {
                     if (n === 1) return reject(error); // <--- base case!
                     RetryableFetch.fetch_retry(request, init, n - 1)
                         .then(resolve)
