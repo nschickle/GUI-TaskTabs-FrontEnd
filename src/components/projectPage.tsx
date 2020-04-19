0a57d3b7c4ece7a46f899f624e11996ce5ebb6cf
Woom <theroyalblah@gmail.com>
2020-04-19 02:56:05 -0400

    resolve merge conflict

--- src/components/projectPage.tsx
+++ src/components/projectPage.tsx
@@ -45,8 +45,10 @@ const testSharedWith: IUser[] = [
 ];
 
 interface ProjectHistory {
-    id: number,
-    name: string
+	id: number,
+	name: string,
+	progress: number,
+	childProgress: number[]
 };
 
 interface ProjectPageProps {
@@ -60,100 +62,99 @@ interface ProjectPageProps {
 // ProjectPage contains the entire application past the Google oauth. This should include the left and right sidebars
 // task view, settings user info, etc.
 export class ProjectPage extends React.Component<ProjectPageProps, { error: any, isLoaded: boolean, task: Task, head: number, history: ProjectHistory[] }>{
-    testVar: number;
 
-    constructor(props: ProjectPageProps) {
-        super(props);
+	constructor(props: ProjectPageProps) {
+		super(props);
 
-        this.state = {
-            error: null,
-            isLoaded: false,
-            task: null,
-            head: undefined,
-            history: [],
-        };
-    }
+		this.state = {
+			error: null,
+			isLoaded: false,
+			task: null,
+			head: undefined,
+			history: [],
+		};
+	}
 
-    // This will attempt to fetch from the database a given number of times.
-    // This is needed because if the head was recently inserted, the fetch will
-    // likely return null as the database will not have caught up yet.
-    makeProjectQuery = (numTries: number) => {
-        let timeout;
-        if (numTries == 0) {
-            this.setState({
-                isLoaded: false,
-                error: true
-            });
-            clearTimeout(timeout);
-        } else {
-            timeout = setTimeout(() => {
-                const request = new UserHeaderHttpRequest(`/api/tasks/${this.props.projectID}`, this.props.userInfo);
-                RetryableFetch.fetch_retry(request)
-                    .then(res => res.json())
-                    .then(
-                        (result) => {
-                            if (result) {
+	// This will attempt to fetch from the database a given number of times.
+	// This is needed because if the head was recently inserted, the fetch will
+	// likely return null as the database will not have caught up yet.
+	makeProjectQuery = (numTries: number) => {
+		let timeout;
+		if (numTries == 0) {
+			this.setState({
+				isLoaded: false,
+				error: true
+			});
+			clearTimeout(timeout);
+		} else {
+			timeout = setTimeout(() => {
+				const request = new UserHeaderHttpRequest(`/api/tasks/${this.props.projectID}`, this.props.userInfo);
+				RetryableFetch.fetch_retry(request)
+					.then(res => res.json())
+					.then(
+						async (result) => {
+							if (result) {
 
-                                // add the project to the beginning of the history
-                                let taskHistoryNode: ProjectHistory;
-                                taskHistoryNode = { id: result._id, name: result.title };
+								// add the project to the beginning of the history
+								let taskHistoryNode: ProjectHistory;
+								let childProgress: number[] = await this.retrieveChildProgress(result._id);
 
-                                // state should be treated as if it were immutable.
-                                // However, concat returns a new array.
-                                this.setState({
-                                    isLoaded: true,
-                                    task: result,
-                                    head: result._id,
-                                    history: this.state.history.concat([taskHistoryNode])
-                                });
+								taskHistoryNode = { id: result._id, name: result.title, progress: result.progress, childProgress: childProgress };
+								// state should be treated as if it were immutable.
+								// However, concat returns a new array.
+								this.setState({
+									isLoaded: true,
+									task: result,
+									head: result._id,
+									history: this.state.history.concat([taskHistoryNode])
+								});
+							} else {
+								this.makeProjectQuery(numTries - 1);
+							}
+						},
+						// Note: it's important to handle errors here
+						// instead of a catch() block so that we don't swallow
+						// exceptions from actual bugs in components.
+						(error) => {
+							this.setState({
+								isLoaded: true,
+								error
+							});
+						}
+					);
+			}, 1000);
+		}
+	}
 
-                            } else {
-                                this.makeProjectQuery(numTries - 1);
-                            }
-                        },
-                        // Note: it's important to handle errors here
-                        // instead of a catch() block so that we don't swallow
-                        // exceptions from actual bugs in components.
-                        (error) => {
-                            this.setState({
-                                isLoaded: true,
-                                error
-                            });
-                        }
-                    );
-            }, 1000);
-        }
-    }
-
-    componentDidMount() {
-        // This will attempt to the make the Project Query 5 times before giving up.
-        this.makeProjectQuery(5);
-    }
+	componentDidMount() {
+		// This will attempt to the make the Project Query 5 times before giving up.
+		this.makeProjectQuery(5);
+	}
 
 
-    // TODO
-    // assignedTo should probably be a User, not a string. Fine for now with dummy data,
-    // but should be replaced.
-    public render() {
+	// TODO
+	// assignedTo should probably be a User, not a string. Fine for now with dummy data,
+	// but should be replaced.
+	public render() {
 
-        const { error, isLoaded, task, head, history } = this.state;
-        // TODO Style error and loading screens
+		const { error, isLoaded, task, head, history } = this.state;
+		// TODO Style error and loading screens
 
-        if (error) {
-            return (
-                <>Error!</>
-            );
-        } else if (!isLoaded) {
-            return (
-                <>Loading...</>
-            );
-        } else {
+		if (error) {
+			return (
+				<>Error!</>
+			);
+		} else if (!isLoaded) {
+			return (
+				<>Loading...</>
+			);
+		} else {
 
-            let historyComponent =
-                <Breadcrumb>
-                    {history.map(node => {
-                        return (
-                            <HistoryButton key={node.id} id={node.id} name={node.name} changeHead={this.changeHeadFromHistory} currentHead={head}
+			let historyComponent =
+				<Breadcrumb>
+					{history.map(node => {
+						return (
+							<HistoryButton key={node.id} id={node.id} name={node.name} changeHead={this.changeHeadFromHistory} currentHead={head}
                             theme = {this.props.theme} fontSize={this.props.fontSize}/>
                         );
                     })}
@@ -186,113 +187,246 @@ export class ProjectPage extends React.Component<ProjectPageProps, { error: any,
                             sharedUsers={testSharedWith}
                             userInfo={this.props.userInfo}
                             theme = {this.props.theme}
-                            fontSize = {this.props.fontSize}
-                            hideProjectPage={this.props.hideProjectPage}
-                        /></Col>
-                        <Col sm="3"><SubTaskColumn head={head} changeHead={this.changeHeadFromTask} userInfo={this.props.userInfo} projectId={this.props.projectID} theme = {this.props.theme} fontSize={this.props.fontSize}/></Col>
-                    </Row>
-                </Container>
-            );
-        }
-    }
+							fontSize = {this.props.fontSize}
+							hideProjectPage={this.props.hideProjectPage}
+							refreshPage={this.refreshPage}
+						/></Col>
+						<Col sm="3"><SubTaskColumn head={head} changeHead={this.changeHeadFromTask} userInfo={this.props.userInfo} projectId={this.props.projectID} theme = {this.props.theme} fontSize={this.props.fontSize}/></Col>
+					</Row>
+				</Container>
+			);
+		}
+	}
 
-    private changeHeadFromHistory = (newHead: number) => {
-        this.changeHead(newHead, false, true);
-    }
+	refreshPage = () => {
+		this.refreshFetch();
+	}
 
-    private changeHeadFromTask = (newHead: number) => {
-        this.changeHead(newHead, false);
-    }
+	private changeHeadFromHistory = (newHead: number) => {
+		this.changeHead(newHead, false, true);
+	}
 
-    private changeHeadFromProject = (newHead: number) => {
-        this.changeHead(newHead, true);
-    }
+	private changeHeadFromTask = (newHead: number) => {
+		this.changeHead(newHead, false);
+	}
 
-    private changeHead = (newHead: number, isProject: boolean, isNewHeadHistoryTail?: boolean) => {
-        const previousHead = this.state.head;
-        if (newHead !== previousHead) {
+	private changeHeadFromProject = (newHead: number) => {
+		this.changeHead(newHead, true);
+	}
 
-            this.setState(() => {
-                return { head: newHead };
-            })
+	// get the progress of all the children and return it as an array
+	private retrieveChildProgress = async (id: number): Promise<number[]> => {
 
-            const request = new UserHeaderHttpRequest(`/api/tasks/${newHead}`, this.props.userInfo);
-            RetryableFetch.fetch_retry(request)
-                .then(res => res.json())
-                .then(
-                    (result) => {
+		let childProgress: number[] = [];
+		//get subtask progress
+		const request = new UserHeaderHttpRequest(`/api/subtasks/${id}`, this.props.userInfo);
+		await fetch(request)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					for (let i = 0; i < result.length; i++) {
+						childProgress.push(result[i].progress);
+					}
+				},
+				(error) => {
+					console.error("cannot retrieve child progress. " + error);
+				}
+			);
 
-                        // Update History
-                        if (isProject) {
-                            let newHistory: ProjectHistory[] = [];
+		return childProgress;
+	}
 
-                            // create new node
-                            let taskHistoryNode: ProjectHistory;
-                            taskHistoryNode = { id: result._id, name: result.title };
+	// refresh fetch, called after a save. The head does not change, but names might have,
+	// so the data needs to be recollected.
+	private refreshFetch = () => {
+		let history = this.state.history;
+		const request = new UserHeaderHttpRequest(`/api/tasks/${this.state.head}`, this.props.userInfo);
+		fetch(request)
+			.then(res => res.json())
+			.then(
+				async (result) => {
 
-                            newHistory.push(taskHistoryNode);
+					let oldHistory = history.pop();
 
-                            // set new task
-                            this.setState({
-                                isLoaded: true,
-                                task: result,
-                                history: newHistory
-                            });
+					let childProgress: number[] = await this.retrieveChildProgress(this.state.head);
 
-                        }
-                        // This deletes the history after the previous head, and adds the new head to the end of the list.
-                        // Reason being that if you have pre-existing history and you go back in history and click a task button that was
-                        // not in the history previous, the entire history after the previous head must be erased.
-                        else if (!isNewHeadHistoryTail) {
-                            let headDiscovered = false;
-                            let history = this.state.history;
-                            let isNewHeadInHistory = false;
+					let taskHistoryNode: ProjectHistory = { id: result._id, name: result.title, progress: result.progress, childProgress: childProgress };
 
-                            // Check if the history is still correct, even though a task button was clicked
-                            for (let i = 0; i < history.length; i++) {
-                                if (history[i].id === newHead) {
-                                    isNewHeadInHistory = true;
-                                }
-                            }
-                            if (!isNewHeadInHistory) {
-                                // destory the end of the history until the head is discovered
-                                while (!headDiscovered && history.length !== 0) {
-                                    if (history[history.length - 1].id !== previousHead) {
-                                        history.pop();
-                                    } else {
-                                        headDiscovered = true;
-                                    }
-                                }
-                                let taskHistoryNode: ProjectHistory;
-                                taskHistoryNode = { id: result._id, name: result.title };
+					history.push(taskHistoryNode);
 
-                                history.push(taskHistoryNode);
-                            }
+					// ensure the child progress in history is up to date
+					// skips the last element
+					for (let i = 0; i < history.length - 1; i++) {
+						if (history[i].childProgress.length === 0) {
+							childProgress = await this.retrieveChildProgress(history[i].id);
+							history[i].childProgress = childProgress;
+						}
+					}
 
-                            this.setState({
-                                isLoaded: true,
-                                task: result,
-                                history: history
-                            });
-                        }
-                        // If the user clicked a history button, no history changes need to be made.
-                        else {
-                            this.setState({
-                                isLoaded: true,
-                                task: result,
-                            });
-                        }
-                    },
-                    // Note: it's important to handle errors here
-                    // instead of a catch() block so that we don't swallow
-                    // exceptions from actual bugs in components.
-                    (error) => {
-                        this.setState({
-                            isLoaded: true,
-                            error
-                        });
-                    }
-                );
-        }
-    }
+					// If the progress of the task changed since the last save
+					if (oldHistory.progress !== taskHistoryNode.progress) {
+						let newAverage;
+						let currentAverage;
+						for (let i = history.length - 1; i >= 0; i--) {
+							newAverage = 0;
+							if (i === history.length - 1) {
+								currentAverage = oldHistory.progress;
+							} else {
+								currentAverage = history[i].progress;
+							}
+
+							// build new average
+							for (let j = 0; j < history[i].childProgress.length; j++) {
+								newAverage += history[i].childProgress[j]
+							}
+							if (history[i].childProgress.length != 0) {
+								newAverage = newAverage / history[i].childProgress.length;
+							} else {
+								newAverage = history[i].progress;
+							}
+
+							if (newAverage !== currentAverage) {
+								// update history
+								history[i].progress = newAverage;
+								// insert new id into database
+								this.updateProgress(history[i].id, newAverage);
+
+								// replace 
+								if (i - 1 >= 0) {
+									for (let j = 0; j < history[i - 1].childProgress.length; j++) {
+										if (history[i - 1].childProgress[j] === currentAverage) {
+											history[i - 1].childProgress[j] = newAverage;
+											break;
+										}
+									}
+								}
+							}
+						}
+					}
+					this.setState({
+						isLoaded: true,
+						task: result,
+						history: history
+					});
+
+				},
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			);
+	}
+
+	private updateProgress = async (id: number, progress: number) => {
+
+		const updatedTask = { owner: this.state.task.owner, title: this.state.task.title, status: this.state.task.status, assignedTo: this.state.task.assignedTo, progress: progress, deadline: this.state.task.deadline, description: this.state.task.description };
+		//get subtask progress
+		const request = new UserHeaderHttpRequest(`/api/tasks/${id}`, this.props.userInfo,  { 'Content-Type': 'application/json' });
+		await fetch(request,
+			{
+				method: 'PUT',
+				mode: 'cors',
+				body: JSON.stringify(updatedTask)
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+
+	}
+
+	private changeHead = (newHead: number, isProject: boolean, isNewHeadHistoryTail?: boolean) => {
+		const previousHead = this.state.head;
+		if (newHead !== previousHead) {
+
+			this.setState(() => {
+				return { head: newHead };
+			})
+
+			const request = new UserHeaderHttpRequest(`/api/tasks/${newHead}`, this.props.userInfo);
+			RetryableFetch.fetch_retry(request)
+				.then(res => res.json())
+				.then(
+					async (result) => {
+
+						// Update History
+						if (isProject) {
+							let newHistory: ProjectHistory[] = [];
+
+							// create new node
+							let taskHistoryNode: ProjectHistory;
+							let childProgress: number[] = await this.retrieveChildProgress(this.state.head);
+
+							taskHistoryNode = { id: result._id, name: result.title, progress: result.progress, childProgress: childProgress };
+
+							newHistory.push(taskHistoryNode);
+
+							// set new task
+							this.setState({
+								isLoaded: true,
+								task: result,
+								history: newHistory
+							});
+							console.log(newHistory);
+
+						}
+						// This deletes the history after the previous head, and adds the new head to the end of the list.
+						// Reason being that if you have pre-existing history and you go back in history and click a task button that was
+						// not in the history previous, the entire history after the previous head must be erased.
+						else if (!isNewHeadHistoryTail) {
+							let headDiscovered = false;
+							let history = this.state.history;
+							let isNewHeadInHistory = false;
+
+							// Check if the history is still correct, even though a task button was clicked
+							for (let i = 0; i < history.length; i++) {
+								if (history[i].id === newHead) {
+									isNewHeadInHistory = true;
+								}
+							}
+							if (!isNewHeadInHistory) {
+								// destory the end of the history until the head is discovered
+								while (!headDiscovered && history.length !== 0) {
+									if (history[history.length - 1].id !== previousHead) {
+										history.pop();
+									} else {
+										headDiscovered = true;
+									}
+								}
+								let taskHistoryNode: ProjectHistory;
+								let childProgress: number[] = await this.retrieveChildProgress(this.state.head);
+
+								taskHistoryNode = { id: result._id, name: result.title, progress: result.progress, childProgress: childProgress };
+
+								history.push(taskHistoryNode);
+								console.log(history);
+							}
+
+							this.setState({
+								isLoaded: true,
+								task: result,
+								history: history
+							});
+						}
+						// If the user clicked a history button, no history changes need to be made.
+						else {
+							this.setState({
+								isLoaded: true,
+								task: result,
+							});
+						}
+					},
+					// Note: it's important to handle errors here
+					// instead of a catch() block so that we don't swallow
+					// exceptions from actual bugs in components.
+					(error) => {
+						this.setState({
+							isLoaded: true,
+							error
+						});
+					}
+				);
+		}
+	}
 };
