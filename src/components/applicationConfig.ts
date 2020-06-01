a300d73c3427a2815eef41510849e9562b6bbdc6
nschickle <nicholasschickle@gmail.com>
2020-05-31 21:45:21 -0400

    Making the changes on release branch

--- src/components/applicationConfig.ts
+++ src/components/applicationConfig.ts
@@ -1,7 +1,7 @@
 export const ApplicationConfig = {
     api: {
         staging: {
-            baseUrl: "https://tasktabs-backend.herokuapp.com",
+            baseUrl: "http://localhost:1337",
         }
     },
     googleAuth: {
